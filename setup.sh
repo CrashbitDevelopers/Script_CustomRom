019ebe444dd1afd7d760976396b6c7276621d978
Felipe de Jesús <felipealcocer30@gmail.com>
2018-01-07 18:58:37 -0600

    Agregada herramienta repo

--- setup.sh
+++ setup.sh
@@ -42,7 +42,7 @@ tput setaf 3
 	echo  
 	sleep 3
 tput setaf 2
-	time sudo apt-get -y install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk3.0-dev \
+	time sudo apt-get -y install git-core repo python gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk3.0-dev \
 squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-8-jre openjdk-8-jdk pngcrush \
 schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev g++-multilib lib32z1-dev lib32ncurses5-dev \
 lib32readline6-dev gcc-multilib liblz4-* pngquant ncurses-dev texinfo gcc patch cmake libtool \
